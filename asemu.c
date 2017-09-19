7885181151af88c9eb50dbd75bbf5dc677bb4051
Andrew Kramer <andrew@jmpesp.org>
2017-09-19 15:13:04 -0500

    initial unicorn engine addition

--- asemu.c
+++ asemu.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <capstone/capstone.h>
+#include <keystone/keystone.h>
+#include <unicorn/unicorn.h>
 
 // WITH the border
 #define CONSOLE_HEIGHT (5 + 2)
@@ -19,8 +22,8 @@
 	"EBP:    %08x\n" \
 	"ESP:    %08x\n" \
 	"EIP:    %08x\n" \
-	"EFLAGS: %08x\n"
 /* TODO
+	"EFLAGS: %08x\n"
 	" CF:%d PF:%d AF:%d ZF:%d\n" \
 	" SF:%d TF:%d IF:%d DF:%d\n" \
 	" OF:%d NT:%d RF:%d VM:%d\n"
@@ -58,6 +61,12 @@ typedef struct registers {
 window_t registers, stack, code, console;
 int parent_y, parent_x;
 
+instruction_t *inst;
+registers_t regs;
+
+ks_engine *ks;
+uc_engine *uc;
+
 void init_window(window_t *w, int y, int x, int height, int width, char *title) {
 
 	w->width = width - 2;
@@ -77,34 +86,47 @@ void draw_window(window_t *w) {
 
 }
 
-void init_regs(registers_t *regs) {
+void init_regs() {
 
-	regs->eax = 0x00000000;
-	regs->ebx = 0x00000000;
-	regs->ecx = 0x00000000;
-	regs->edx = 0x00000000;
-	regs->esi = 0x00000000;
-	regs->edi = 0x00000000;
-	regs->ebp = 0xffffffff;
-	regs->esp = 0xffffffff;
-	regs->eip = 0x00400000;
+	regs.eax = 0x00000000;
+	regs.ebx = 0x00000000;
+	regs.ecx = 0x00000000;
+	regs.edx = 0x00000000;
+	regs.esi = 0x00000000;
+	regs.edi = 0x00000000;
+	regs.ebp = 0xffffffff;
+	regs.esp = 0xffffffff;
+	regs.eip = 0x00400000;
+
+	uc_reg_write(uc, UC_X86_REG_EAX, &regs.eax);
+	uc_reg_write(uc, UC_X86_REG_EBX, &regs.ebx);
+	uc_reg_write(uc, UC_X86_REG_ECX, &regs.ecx);
+	uc_reg_write(uc, UC_X86_REG_EDX, &regs.edx);
+	uc_reg_write(uc, UC_X86_REG_ESI, &regs.esi);
+	uc_reg_write(uc, UC_X86_REG_EDI, &regs.edi);
+	uc_reg_write(uc, UC_X86_REG_EBP, &regs.ebp);
+	uc_reg_write(uc, UC_X86_REG_ESP, &regs.esp);
+	uc_reg_write(uc, UC_X86_REG_EIP, &regs.eip);
+
 
 }
 
-void init_instructions(instruction_t **inst, char *file, int entrypoint) {
+void init_instructions(char *file, int entrypoint) {
 
 	FILE *fp;
 	char buff[1024];
+	unsigned char *opcodes;
+	size_t size, count;
 	int i;
 	instruction_t *tmp;
 
 	fp = fopen(file, "r");
 	if(!fp) {
-		*inst = NULL;
+		inst = NULL;
 		return;
 	}
 
-	tmp = *inst;
+	tmp = inst;
 
 	while(fgets(buff, 1024, fp)) {
 
@@ -115,26 +137,35 @@ void init_instructions(instruction_t **inst, char *file, int entrypoint) {
 			}
 		}
 
-		if(*inst == NULL) {
-			*inst = malloc(sizeof(instruction_t));
-			(*inst)->prev = NULL;
-			(*inst)->next = NULL;
-			(*inst)->address = entrypoint++;
-			strncpy((*inst)->opcodes, "\x41\x42\x43", 4);
-			(*inst)->opcode_len = 3;
-			strncpy((*inst)->text, buff, 32);
+		if(ks_asm(ks, buff, 0, &opcodes, &size, &count) != KS_ERR_OK) {
+			printf("ERROR: Failed to assemble instruction '%s'\n", buff);
+			return;
+		}
+
+		if(inst == NULL) {
+			inst = malloc(sizeof(instruction_t));
+			inst->prev = NULL;
+			inst->next = NULL;
+			inst->address = entrypoint;
+			memcpy(inst->opcodes, opcodes, size);
+			inst->opcode_len = size;
+			strncpy(inst->text, buff, 32);
 		} else {
 			if(!tmp)
-				tmp = *inst;
+				tmp = inst;
 			tmp->next = malloc(sizeof(instruction_t));
 			tmp = tmp->next;
 			tmp->prev = tmp;
 			tmp->next = NULL;
-			tmp->address = entrypoint++;
-			strncpy(tmp->opcodes, "\x41\x42\x43", 4);
-			tmp->opcode_len = 3;
+			tmp->address = entrypoint;
+			memcpy(tmp->opcodes, opcodes, size);
+			tmp->opcode_len = size;
 			strncpy(tmp->text, buff, 32);
 		}
+
+		entrypoint += size;
+
+		free(opcodes);
 
 	}
 
@@ -142,10 +173,22 @@ void init_instructions(instruction_t **inst, char *file, int entrypoint) {
 
 }
 
+void init_memory() {
+
+	instruction_t *tmp;
+
+	uc_mem_map(uc, regs.eip, 2 * 1024 * 1024, UC_PROT_ALL);
+
+	for(tmp=inst; tmp; tmp=tmp->next) {
+		uc_mem_write(uc, tmp->address, tmp->opcodes, tmp->opcode_len);
+	}
+
+}
+
 void usage(char *arg0) {
 
 	printf("Usage: %s [OPTIONS] <file.s>\n", arg0);
-	printf("A simple 32-bit x86 interpreter\n");
+	printf("A simple 32-bit x86 emulator\n");
 	printf("\n");
 	printf("  %-10s %s\n", "-h", "Print this help menu");
 	printf("\n");
@@ -153,13 +196,52 @@ void usage(char *arg0) {
 
 }
 
-int main(int argc, char *argv[]) {
+void render() {
 
-	instruction_t *inst, *tmp;
-	registers_t regs;
-	int opt;
 	int i, j;
 	char opcode[4], opcodes[32];
+	instruction_t *tmp;
+
+	uc_reg_read(uc, UC_X86_REG_EAX, &regs.eax);
+	uc_reg_read(uc, UC_X86_REG_EBX, &regs.ebx);
+	uc_reg_read(uc, UC_X86_REG_ECX, &regs.ecx);
+	uc_reg_read(uc, UC_X86_REG_EDX, &regs.edx);
+	uc_reg_read(uc, UC_X86_REG_ESI, &regs.esi);
+	uc_reg_read(uc, UC_X86_REG_EDI, &regs.edi);
+	uc_reg_read(uc, UC_X86_REG_EBP, &regs.ebp);
+	uc_reg_read(uc, UC_X86_REG_ESP, &regs.esp);
+	uc_reg_read(uc, UC_X86_REG_EIP, &regs.eip);
+
+	mvwprintw(registers.content, 0, 0, REGISTERS_FMT,
+		regs.eax, regs.ebx, regs.ecx, regs.edx,
+		regs.esi, regs.edi, regs.ebp, regs.esp,
+		regs.eip);
+	draw_window(&registers);
+
+	draw_window(&stack);
+
+	for(i=0,tmp=inst; tmp; i++,tmp=tmp->next) {
+		memset(opcodes, '\0', 32);
+		for(j=0; j<tmp->opcode_len; j++) {
+			sprintf(opcode, "%02hhx ", tmp->opcodes[j]);
+			strcat(opcodes, opcode);
+		}
+		mvwprintw(code.content, i, 0,
+			"%c %08x %-16s %s\n",
+			(tmp->address==regs.eip ? '>' : ' '),
+			tmp->address, opcodes, tmp->text);
+	}
+	draw_window(&code);
+
+	draw_window(&console);
+
+	wmove(console.content, 0, 0);
+
+}
+
+int main(int argc, char *argv[]) {
+
+	int opt;
 
 	while((opt = getopt(argc, argv, "h")) != -1) {
 		switch(opt) {
@@ -181,6 +263,16 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
+	if(ks_open(KS_ARCH_X86, KS_MODE_32, &ks) != KS_ERR_OK) {
+		printf("ERROR: Failed to initialize Keystone engine\n");
+		return 0;
+	}
+
+	if(uc_open(UC_ARCH_X86, UC_MODE_32, &uc) != UC_ERR_OK) {
+		printf("ERROR: Failed to initialize Unicorn engine\n");
+		return 0;
+	}
+
 	initscr();
 
 	getmaxyx(stdscr, parent_y, parent_x);
@@ -190,44 +282,26 @@ int main(int argc, char *argv[]) {
 	init_window(&code, 0, REGISTER_WIDTH, parent_y - CONSOLE_HEIGHT, parent_x - REGISTER_WIDTH - STACK_WIDTH, "CODE");
 	init_window(&console, parent_y - CONSOLE_HEIGHT, 0, CONSOLE_HEIGHT, parent_x, "CONSOLE");
 
-	init_regs(&regs);
+	init_regs();
 
 	inst = NULL;
-	init_instructions(&inst, argv[optind], regs.eip);
+	init_instructions(argv[optind], regs.eip);
 
 	if(!inst) {
 		printf("ERROR: failed to parse instructions\n");
 		return 0;
 	}
 
+	init_memory();
+
 	char buff[1024];
 	for(;;) {
 
-		mvwprintw(registers.content, 0, 0, REGISTERS_FMT,
-			regs.eax, regs.ebx, regs.ecx, regs.edx,
-			regs.esi, regs.edi, regs.ebp, regs.esp,
-			regs.eip);
-		draw_window(&registers);
+		render();
 
-		draw_window(&stack);
-
-		for(i=0,tmp=inst; tmp; i++,tmp=tmp->next) {
-			memset(opcodes, '\0', 32);
-			for(j=0; j<tmp->opcode_len; j++) {
-				sprintf(opcode, "%02hhx ", tmp->opcodes[j]);
-				strcat(opcodes, opcode);
-			}
-			mvwprintw(code.content, i, 0,
-				"%c %08x %16s %s\n",
-				(tmp->address==regs.eip ? '>' : ' '),
-				tmp->address, opcodes, tmp->text);
-		}
-		draw_window(&code);
-
-		draw_window(&console);
-
-		wmove(console.content, 0, 0);
 		wgetnstr(console.content, buff, 1024);
+
+		uc_emu_start(uc, regs.eip, 0xffffffff, 0, 1);
 
 	}
 
